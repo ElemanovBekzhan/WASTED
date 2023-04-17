ada6e46cc0263e519b7dac1e8a0e1a7db1b03dd0
ElemanovBekzhan <94730786+ElemanovBekzhan@users.noreply.github.com>
2023-04-17 10:46:13 +0600

    Create Menu

--- Company.Designer.cs
+++ Company.Designer.cs
@@ -1,11 +1,13 @@
-﻿namespace WindowsFormsApp2Tereksay
+using System.ComponentModel;
+
+namespace WindowsFormsApp2Tereksay
 {
-    partial class Form1
+    partial class Company
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -32,7 +34,7 @@
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Company";
         }
 
         #endregion
